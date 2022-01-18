3398b85474f9d8b23243295d6f3cadcd438fe702
Dominik Konopka <colonel@interia.pl>
2022-01-18 01:27:32 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(WhoLikesIt.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+
+
+    }
+}
